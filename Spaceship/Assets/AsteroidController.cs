af6732a4657c56dd07a96d91bc697f5bc3d22bfd
joshmcgee <talidos.josh@gmail.com>
2014-11-17 03:01:52 -0500

    Added asteroids and updated collision layers.

---
+++ Spaceship/Assets/AsteroidController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour {
+
+	public float minRotationSpeed;
+	public float maxRotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360)));
+
+		ApplyRandomRotation(minRotationSpeed, maxRotationSpeed);
+	}
+
+	void ApplyRandomRotation (float min, float max) {
+		float speed = Random.Range(min, max);
+		if (Random.value > 0.5f) {
+			speed *= -1;
+		}
+
+		rigidbody2D.angularVelocity = speed;
+	}
+}
